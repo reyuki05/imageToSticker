1fc7df6850473e37f6b123b7cd54a692060e01a3
reyuki05 <81926892+reyuki05@users.noreply.github.com>
2021-04-05 02:11:04 +0700

    Update mention.js

--- utils/mention.js
+++ utils/mention.js
@@ -8,3 +8,17 @@
  * @param  {Array} list - group members
  * @return {Array} Return processed array
  */
+module.exports = mentionList = (id, botNumber, list) => {
+    let mentionlist = ''
+    list.map((x) => {
+        if ((x !== id) && (x !== botNumber)) mentionlist += `@${x.replace('@c.us', '')} `
+    })
+    return mentionlist
+}
+
+// Script untuk file index.js
+
+if (!isGroupMsg) return client.reply(from, 'Maaf, perintah ini hanya dapat dipakai didalam grup! [Group Only]', id)
+if (!isGroupAdmins) return client.reply(from, 'Gagal, perintah ini hanya dapat digunakan oleh admin grup! [Admin Group Only]', id)
+const mentions = mentionList(sender.id, botNumber, groupMembers)
+await client.sendTextWithMentions(from, `Heyy, ${pushname} is calling you !!!\n${mentions}`)
